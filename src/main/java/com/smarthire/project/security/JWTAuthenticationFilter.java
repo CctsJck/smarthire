a82df443c8ca3f5612564d66f1f068f9b2f04ddf
CctsJck <julipro99@gmail.com>
2023-07-21 16:52:55 -0300

    half jwt

--- src/main/java/com/smarthire/project/security/JWTAuthenticationFilter.java
+++ src/main/java/com/smarthire/project/security/JWTAuthenticationFilter.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,7 +21,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         AuthCredentials authCredentials = new AuthCredentials();
         try{
-            authCredentials = new ObjectMapper().readValue(request.getReader(), AuthCredentials.class)
+            authCredentials = new ObjectMapper().readValue(request.getReader(), AuthCredentials.class);
         } catch (IOException e){
         }
 
@@ -30,5 +32,20 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         );
 
         return getAuthenticationManager().authenticate(usernamePAT);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        UserDetailsImpl userDetails = (UserDetailsImpl)authResult.getPrincipal();
+        String token = TokenUtils.createToken(userDetails.getName(), userDetails.getUsername());
+
+        response.addHeader("Authorization", "Bearer "+ token);
+        response.getWriter().flush();
+
+
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
--- src/main/java/com/smarthire/project/security/WebSecurityConfig.java
+++ src/main/java/com/smarthire/project/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.smarthire.project.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,29 +12,46 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.net.PasswordAuthentication;
+import java.sql.SQLOutput;
+
+import static org.springframework.http.HttpMethod.POST;
+
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
+
+
+    private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+
+
 
     @Bean
     SecurityFilterChain filterChain (HttpSecurity http, AuthenticationManager authManager) throws Exception {
+        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
+        jwtAuthenticationFilter.setAuthenticationManager(authManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+
         return http
                 .csrf().disable()
                 .authorizeRequests()
+                    .antMatchers("/recruiter/**").permitAll()
                 .anyRequest()
                 .authenticated()
-                .and()
-                .httpBasic()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter,UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-    @Bean
+   /* @Bean
     UserDetailsService userDetailsService(){
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("admin")
@@ -42,12 +60,12 @@ public class WebSecurityConfig {
                 .build());
 
         return manager;
-    }
+    }*/
 
     @Bean
     AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService())
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder())
                 .and()
                 .build();
@@ -57,5 +75,6 @@ public class WebSecurityConfig {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 
 }
