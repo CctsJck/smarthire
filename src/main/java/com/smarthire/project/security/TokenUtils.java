012da49969f258c6866c8ee81cfb78cfa778b9ee
CctsJck <julipro99@gmail.com>
2023-10-29 13:19:10 -0300

    back to previous commit

--- src/main/java/com/smarthire/project/security/TokenUtils.java
+++ src/main/java/com/smarthire/project/security/TokenUtils.java
@@ -4,40 +4,28 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.beans.factory.annotation.Value;
 
-import javax.annotation.PostConstruct;
 import java.security.Key;
 import java.util.*;
-@Slf4j
+
 public class TokenUtils {
-    @Value("${JAVA_PRUEBA_STRING}")
-    private static String ACCESS_TOKEN_SECRET;
+
+    private final static String ACCESS_TOKEN_SECRET = "qf4243omf234mfdmfmi2i34fmdm3imdmwif4";
     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 
-    @Value("${JAVA_PRUEBA_STRING}")
-    private String accessTokenSecretFromProperty;
-
-    @PostConstruct
-    public void initializeAccessTokenSecret() {
-        log.info(accessTokenSecretFromProperty);
-        ACCESS_TOKEN_SECRET = accessTokenSecretFromProperty;
-    }
-
     public static String createToken(String name, String email){
-         long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1_000;
-         Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+        long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1_000;
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 
-         Map<String, Object> extra = new HashMap<>();
-         extra.put("name", name);
-         return Jwts.builder()
-                 .setSubject(email)
-                 .setExpiration(expirationDate)
-                 .addClaims(extra)
-                 .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
-                 .compact();
+        Map<String, Object> extra = new HashMap<>();
+        extra.put("name", name);
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(expirationDate)
+                .addClaims(extra)
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()))
+                .compact();
     }
 
     public static UsernamePasswordAuthenticationToken getAuthentication(String token){
