59fd7f982da2c92a30cc01daddad516a9f1f94af
CctsJck <julipro99@gmail.com>
2023-10-29 16:46:45 -0300

    New env var

--- src/main/java/com/smarthire/project/security/TokenUtils.java
+++ src/main/java/com/smarthire/project/security/TokenUtils.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class TokenUtils {
 
-    private final static String ACCESS_TOKEN_SECRET = "qf4243omf234mfdmfmi2i34fmdm3imdmwif4";
+    private final static String ACCESS_TOKEN_SECRET = System.getenv("ACCESS_TOKEN_SECRET");
     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
 
     public static String createToken(String name, String email){
